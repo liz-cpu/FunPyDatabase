bad66d62901206d1df524622eedac83985813221
liz-cpu <40073038+liz-cpu@users.noreply.github.com>
2021-05-23 03:20:26 +0200

    ✨ Test setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='FunDatabaseStuff',
+   version='1.0',
+   description='A unhelpful module',
+   author='emsquellen',
+   packages=['FunDatabaseStuff']
+)
